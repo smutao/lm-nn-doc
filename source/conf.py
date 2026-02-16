c8d5d0ab6f7e6295d43a5dcfe9abeb2f25cdbced
YT <ywtao.smu@gmail.com>
2026-02-16 00:16:55 -0800

    Update copyright year to 2026

--- source/conf.py
+++ source/conf.py
@@ -18,7 +18,7 @@
 # -- Project information -----------------------------------------------------
 
 project = 'LModeA-nano'
-copyright = '2022-2025'
+copyright = '2022-2026'
 author = 'YT'
 
 # The full version, including alpha/beta/rc tags
